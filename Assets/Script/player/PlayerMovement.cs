52f851234e4c6164d450bcd767b1831b48ec4567
Suvamjyoti Panda <spanda@xansrtech.com>
2021-08-17 20:11:13 +0530

    final PlayerMovement Controller.

--- Assets/Script/player/PlayerMovement.cs
+++ Assets/Script/player/PlayerMovement.cs
@@ -7,10 +7,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float rotationSpeed;
 
-    private float sensitivity = 10f;
-    private Vector2 turn;
-
-
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -23,13 +19,7 @@ public class PlayerMovement : MonoBehaviour
         float _translation = Input.GetAxisRaw("Vertical");
         float _rotation = Input.GetAxisRaw("Horizontal");
 
-        turn.x = Input.GetAxis("Mouse X") * sensitivity;
-        turn.y = Input.GetAxis("Mouse Y") * sensitivity;
-
-        //transform.localRotation = Quaternion.Euler(0,-turn.y, 0);
-
         transform.Translate(_translation*speed*Time.deltaTime,0,0);
-        //transform.Rotate(0, _rotation * rotationSpeed * Time.deltaTime, 0);
-        transform.Rotate(0,turn.y, 0);
+        transform.Rotate(0, _rotation * rotationSpeed * Time.deltaTime, 0);
     }
 }
