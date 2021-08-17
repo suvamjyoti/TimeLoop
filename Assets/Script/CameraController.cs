151a22b91180ef5b2e40f6c4bb6137804230ed88
Suvamjyoti Panda <spanda@xansrtech.com>
2021-08-17 09:15:33 +0530

    [+] crude camera and player controller.

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private Vector3 rotationOffset;
+
+
+
+    private void Update()
+    {
+        transform.position = target.transform.position + offset;
+        transform.rotation = Quaternion.Euler(target.transform.localEulerAngles + rotationOffset);
+    }
+
+}
---
+++ Assets/Script/player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float rotationSpeed;
+
+    private float sensitivity = 10f;
+    private Vector2 turn;
+
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void Update()
+    {
+
+        float _translation = Input.GetAxisRaw("Vertical");
+        float _rotation = Input.GetAxisRaw("Horizontal");
+
+        turn.x = Input.GetAxis("Mouse X") * sensitivity;
+        turn.y = Input.GetAxis("Mouse Y") * sensitivity;
+
+        //transform.localRotation = Quaternion.Euler(0,-turn.y, 0);
+
+        transform.Translate(_translation*speed*Time.deltaTime,0,0);
+        //transform.Rotate(0, _rotation * rotationSpeed * Time.deltaTime, 0);
+        transform.Rotate(0,turn.y, 0);
+    }
+}
