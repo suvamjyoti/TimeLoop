672e616cd9de991acee5fde354b070599eef8cbb
Suvamjyoti Panda <spanda@xansrtech.com>
2021-08-17 21:18:33 +0530

    camera mouse movement.

---
+++ Assets/Script/Camera/CamTargetController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamTargetController : MonoBehaviour
+{
+    [SerializeField]private float mouseSensititvity;
+    private float x;
+    
+
+    private void Update()
+    {
+        x += Input.GetAxisRaw("Mouse X") * mouseSensititvity;
+        
+        if(x > 0 && x < 180)
+        {
+            transform.localRotation = Quaternion.Euler(0, x, 0);
+        }
+
+    }
+
+}
